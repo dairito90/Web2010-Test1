78ebef74632aaa03be0d6768a7b11fa12e33e839
Dairon Rodriguez <drodriguez@fvi.edu>
2016-11-29 18:27:18 -0500

    test 1 js-complete

---
+++ Anwsers.js
@@ -0,0 +1,30 @@
+ //question1
+ var people = [ ['jose', 28], ['francis', 20], ['henrietta', 25],['John',35] ];
+people[6]='John';
+people[7]=35;
+
+
+//question2
+var food = [ ['banana', 'lemon', 'strawberry'], ['almond', 'pecan', 'pistachio'], ['carrot', 'potato', 'beet'] ];
+food[7]='potato';
+
+
+
+//questsion3
+var a = " and I create web applications."
+var job = "Web Developer";
+var b = "Hello, I am a ";
+
+"var b" + " " +  " var job" + " " + "var a"
+
+//question4
+FirstLine
+'SecondLine'
+    ThirdLine
+
+var MyStr = FirstLine\n\t'SecondLine'\\ThirdLine;
+
+
+
+
+//question5
