eb9f246a2fdcaf4818d54f06dcc24862fd59e68f
BrentonCozby <brenton.cozby@gmail.com>
2016-11-30 17:59:58 -0500

    grade test

--- Anwsers.js
+++ Anwsers.js
@@ -1,28 +1,40 @@
- //question1
+ //question1 - 0/2
  var people = [ ['jose', 28], ['francis', 20], ['henrietta', 25],['John',35] ];
 people[6]='John';
 people[7]=35;
+// Correct Answer:
+people.push(['John', 35]);
+// You were not supposed to edit the people array directly; supposed to only use the push method.
 
-
-//question2
+//question2 - 0/2
 var food = [ ['banana', 'lemon', 'strawberry'], ['almond', 'pecan', 'pistachio'], ['carrot', 'potato', 'beet'] ];
 food[7]='potato';
+// Correct answer:
+console.log(food[2][1]);
 
 
 
-//questsion3
+//questsion3 - 1/2
 var a = " and I create web applications."
 var job = "Web Developer";
 var b = "Hello, I am a ";
 
 "var b" + " " +  " var job" + " " + "var a"
+// Correct Answer:
+console.log(b + job + a);
+// You get partial credit because the got the order of the variables correct
 
-//question4
+
+
+//question4 - 1/2
 FirstLine
 'SecondLine'
     ThirdLine
 
 var MyStr = FirstLine\n\t'SecondLine'\\ThirdLine;
+// Correct Answer:
+"Firstline\n\'SecondLine\'\n\tThirdline";
+// You get partial credit because you did some things correctly, like the new line
 
 
 
